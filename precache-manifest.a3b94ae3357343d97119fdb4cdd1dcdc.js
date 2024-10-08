cb1e10895a5f308703aa9d2956df73d185a5d373
Ayeshachaudhry <ayeshachaudhry58@gmail.com>
2024-10-08 16:35:57 +0500

    Updates

---
+++ precache-manifest.a3b94ae3357343d97119fdb4cdd1dcdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ab691bf61736c995c63e073c9e553d",
+    "url": "/Weather-redux-app/index.html"
+  },
+  {
+    "revision": "1bbbb80e88acd01fa7ce",
+    "url": "/Weather-redux-app/static/css/main.36fcb1b7.chunk.css"
+  },
+  {
+    "revision": "4559ef38f51554131bfe",
+    "url": "/Weather-redux-app/static/js/2.09f37c5a.chunk.js"
+  },
+  {
+    "revision": "1bbbb80e88acd01fa7ce",
+    "url": "/Weather-redux-app/static/js/main.9191482c.chunk.js"
+  },
+  {
+    "revision": "7ebecb6d4477830d2288",
+    "url": "/Weather-redux-app/static/js/runtime~main.e49c781c.js"
+  }
+]);
